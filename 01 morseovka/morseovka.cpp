9ae7bb5c0f3dade12e667a6691c297be57662c2c
Tomas_Erlebach <t.erleb@seznam.cz>
2022-03-02 22:55:25 +0100

    Prevod do morseova kodu

--- 01 morseovka/morseovka.cpp
+++ 01 morseovka/morseovka.cpp
@@ -7,17 +7,108 @@
 #define SYMBOL_SPACE     500ms // pauza za znakem
 #define LETTER_SPACE    1500ms // mezera mezi slovy jako tri tecky
 
+/// <summary>Prevede znak na odpovidajici morseuv kod (zachova mezery)</summary>
+string morseEncode(char x)
+{
+    if (isupper(x)) {
+        tolower(x); // prevede velke na male pismeno
+    }
+
+    switch (x) {
+    case 'a':
+        return ".-";
+    case 'b':
+        return "-...";
+    case 'c':
+        return "-.-.";
+    case 'd':
+        return "-..";
+    case 'e':
+        return ".";
+    case 'f':
+        return "..-.";
+    case 'g':
+        return "--.";
+    case 'h':
+        return "....";
+    case 'i':
+        return "..";
+    case 'j':
+        return ".---";
+    case 'k':
+        return "-.-";
+    case 'l':
+        return ".-..";
+    case 'm':
+        return "--";
+    case 'n':
+        return "-.";
+    case 'o':
+        return "---";
+    case 'p':
+        return ".--.";
+    case 'q':
+        return "--.-";
+    case 'r':
+        return ".-.";
+    case 's':
+        return "...";
+    case 't':
+        return "-";
+    case 'u':
+        return "..-";
+    case 'v':
+        return "...-";
+    case 'w':
+        return ".--";
+    case 'x':
+        return "-..-";
+    case 'y':
+        return "-.--";
+    case 'z':
+        return "--..";
+    case '1':
+        return ".----";
+    case '2':
+        return "..---";
+    case '3':
+        return "...--";
+    case '4':
+        return "....-";
+    case '5':
+        return ".....";
+    case '6':
+        return "-....";
+    case '7':
+        return "--...";
+    case '8':
+        return "---..";
+    case '9':
+        return "----.";
+    case '0':
+        return "-----";
+    default:
+        return " ";
+    }
+}
+
+// <summary>zakoduje retezec do morseova kodu </summary>
+string morseCode(string s) {
+    int lenght = s.length();
+    string str;
+
+    for (int i = 0; i < lenght; i++) {
+
+        str.append(morseEncode(s[i]));           
+    }
+    
+    return str;
+}
+
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
-
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(BLINKING_LONG);
-    }
+    string s = "Karel Drnec a Tomas Erlebach";
+        
+    string str = morseCode(s);
+    //cout << str << endl;
 }
