4ea72a2d9ef2d79443ceb77da167c54107614489
Tomas_Erlebach <t.erleb@seznam.cz>
2022-03-07 12:05:50 +0100

    Update morseovka.cpp
    Morse code

--- 01 morseovka/morseovka.cpp
+++ 01 morseovka/morseovka.cpp
@@ -1,11 +1,18 @@
 /* MIP 2022 Drnec Erlebach */
 #include "mbed.h"
+#include <string>;
+using namespace std;
+
+
+//=====================================
 
 // Blinking rate in milliseconds
-#define BLINKING_SHORT    500ms
-#define BLINKING_LONG    1500ms // mazera jako 3 tečky
-#define SYMBOL_SPACE     500ms // pauza za znakem
-#define LETTER_SPACE    1000ms // 2x SYMBOL_SPACE (např. 500 + 1000, mezera mezi slovy jako tri tecky)
+#define BLINKING_RATE    500ms
+
+#define BLINKING_SHORT 1
+#define BLINKING_LONG    3 // mazera jako 3 tečky
+#define SYMBOL_SPACE     1 // pauza za znakem
+#define LETTER_SPACE    2 // 2x SYMBOL_SPACE (např. 500 + 1000, mezera mezi slovy jako tri tecky)
 
 /// <summary>Prevede znak na odpovidajici morseuv kod (zachova mezery)</summary>
 string morseEncode(char x)
@@ -99,15 +106,46 @@ string morseCode(string s) {
 
     for (int i = 0; i < length; i++) {
 
-        str.append(morseEncode(s[i]));           
+        str.append(morseEncode(s[i]));
     }
-    
+
     return str;
 }
 
+// <summary> Funkce realizující bliknutí </summary>
+void blink(DigitalOut led, int length) {
+
+    led = 1;
+    for (int i = 0; i < length; i++) {
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
+void turnOffLed(DigitalOut led, int length) {
+    led = 0;
+    for (int i = 0; i < length; i++) {
+        ThisThread::sleep_for(BLINKING_RATE);
+    }
+}
+
+// <summary> Nastaví interval bliknutí podle znaku </summary>
+void blinkChar(DigitalOut led, char ch) {
+
+    switch (ch) {
+    case '.': blink(led, BLINKING_SHORT);
+        turnOffLed(led, SYMBOL_SPACE);
+    case '-': blink(led, BLINKING_LONG);
+        turnOffLed(led, SYMBOL_SPACE);
+    default:
+        turnOffLed(led, LETTER_SPACE);
+
+    }
+
+}
+
+// <summary> Realizuje blikání dle morseova kódu </summary>
 void blinkSwitch(DigitalOut led, string code) {
 
-    int length = code.length();  
+    int length = code.length();
 
     for (int i = 0; i < length; i++) {
 
@@ -116,46 +154,19 @@ void blinkSwitch(DigitalOut led, string code) {
 
 }
 
-
-void blinkChar(DigitalOut led,char ch) {
-
-    switch (ch) {    
-    case '.': blink(led, BLINKING_SHORT);
-              turnOffLed(led, SYMBOL_SPACE);
-    case '-': blink(led, BLINKING_LONG);
-              turnOffLed(led, SYMBOL_SPACE);
-    default:
-        turnOffLed(led, LETTER_SPACE);
-       
-    }
-
-
-}
-
-void blink(DigitalOut led, BLINKING_RATE) {
-
-    led = 1;
-    ThisThread::sleep_for(BLINKING_RATE);
-    
-}
-void turnOffLed(DigitalOut led, RATE) {
-    led = 0;
-    ThisThread::sleep_for(RATE);
-}
-
 int main()
 {
-    #ifdef LED1
-        DigitalOut led(LED1);
-    #else
-        bool led;
-    #endif
+#ifdef LED1
+    DigitalOut led(LED1);
+#else
+    bool led;
+#endif
 
 
     string s = "Karel Drnec a Tomas Erlebach";
-        
+
     string str = morseCode(s);
-    blinkSwitch(led,str);
+    blinkSwitch(led, str);
 
 
     //cout << str << endl;
