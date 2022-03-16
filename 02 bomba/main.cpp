e8be3d52c4238768b4b5d8779fb58843a4338fd3
kareldrnec <kareldrnec@gmail.com>
2022-03-16 15:59:06 +0100

    Update main.cpp
    update main

--- 02 bomba/main.cpp
+++ 02 bomba/main.cpp
@@ -1,7 +1,6 @@
 #include "mbed.h"
 #include "stm32746g_discovery_lcd.h"
 #include "stm32746g_discovery_ts.h"
-#include "stlogo.h"
 
 using namespace std::chrono;
 
@@ -16,12 +15,16 @@ using namespace std::chrono;
 #define incY 120
 #define decY 180
 
+InterruptIn button(BUTTON1);
 Timeout flipper;
+TS_StateTypeDef TS_State;
+int timeToAdd;
 
-void drawButtons() {
-    // set     
+void drawButtons()
+{
+    // set
     BSP_LCD_DrawRect(setX, setY, setWidth, setHeight);
-    BSP_LCD_DisplayStringAt(0, 135, (uint8_t *)"Set", CENTER_MODE);    
+    BSP_LCD_DisplayStringAt(0, 135, (uint8_t *)" ", CENTER_MODE);
     // inc
     BSP_LCD_DrawRect(incDecX, incY, incDecSize, incDecSize);
     BSP_LCD_DisplayChar(417, 135, '+');
@@ -31,46 +34,54 @@ void drawButtons() {
 
 }
 
-void drawBomb() {
-    // draw a bomb    
+void drawBomb()
+{
+    // draw a bomb
     BSP_LCD_FillCircle(55, 210, 50);
     BSP_LCD_DrawVLine(55, 130, 30);
     BSP_LCD_SetTextColor(LCD_COLOR_YELLOW);
-    BSP_LCD_FillCircle(55, 120, 10);    
+    BSP_LCD_FillCircle(55, 120, 10);
     BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
 }
 
-void redrawWick(int remaining_time) {
-    // TODO    
-}
-
-void boomAnimation(int count) {
-    for(int i = 0; i < count; i++){
+void boomAnimation(int count)
+{
+    for(int i = 0; i < count; i++) {
         BSP_LCD_Clear(LCD_COLOR_RED);
-        BSP_LCD_SetFont(&Font24); 
+        BSP_LCD_SetFont(&Font24);
         BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"BOOM!", CENTER_MODE);
         HAL_Delay(500);
         BSP_LCD_ClearStringLine(5);
         BSP_LCD_Clear(LCD_COLOR_DARKGRAY);
-        BSP_LCD_SetFont(&Font24); 
+        BSP_LCD_SetFont(&Font24);
         BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"PRASK!", CENTER_MODE);
-        HAL_Delay(500);   
+        HAL_Delay(500);
     }
     BSP_LCD_Clear(LCD_COLOR_WHITE);
 }
 
-void display() {    
+void display()
+{
     boomAnimation(4);
     HAL_Delay(1000);
     drawButtons();
     drawBomb();
-    flipper.attach(&display, 10s);
+    flipper.attach(&display, 120s);
+}
+
+void set()
+{ 
+    uint8_t text2[30];
+    HAL_Delay(1000);
+    flipper.detach();
+    flipper.attach(&set, timeToAdd + duration_cast<seconds>(flipper.remaining_time()).count() + 1);
 }
 
 int main()
 {
-    TS_StateTypeDef TS_State;
+    int isTouch = 0;
     int timeValue;
+    timeToAdd = 0;
     uint16_t x, y;
     uint8_t text[30];
     uint8_t status;
@@ -99,28 +110,32 @@ int main()
     }
     HAL_Delay(1000);
     BSP_LCD_Clear(LCD_COLOR_WHITE);
-    BSP_LCD_SetFont(&Font24);  
+    BSP_LCD_SetFont(&Font24);
     BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
     BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
-    
+
     // draw buttons
     drawButtons();
-    
+
     // draw bomba
     drawBomb();
-    
-    flipper.attach(&display, 10s);
-    
+
+    flipper.attach(&display, 100s);
+
+    button.rise(&set);
+
     while(1) {
-       // print zbyvajiciho casu
-        timeValue = duration_cast<seconds>(flipper.remaining_time()).count() + 1;
-        sprintf((char*)text, "%llu seconds remaining!!!", duration_cast<seconds>(flipper.remaining_time()).count() + 1);
         
-        // clear a display string
-        if(timeValue == 9) {
-            BSP_LCD_ClearStringLine(1); 
-        }
-        BSP_LCD_DisplayStringAt(0, LINE(1), (uint8_t *)&text, CENTER_MODE);
+        timeValue = duration_cast<seconds>(flipper.remaining_time()).count() + 1;
+        BSP_LCD_DisplayStringAt(0, LINE(1), (uint8_t *)"Remaining Time", CENTER_MODE);
+
+        sprintf((char*)text, "  %d  ", timeToAdd);
+        
+        BSP_LCD_DisplayStringAt(0, 135, (uint8_t *)&text, CENTER_MODE);
+
+        sprintf((char*)text, "  %llu  ", duration_cast<seconds>(flipper.remaining_time()).count() + 1);
+
+        BSP_LCD_DisplayStringAt(0, LINE(2), (uint8_t *)&text, CENTER_MODE);
 
         // touch detect
         BSP_TS_GetState(&TS_State);
@@ -128,14 +143,15 @@ int main()
             for(idx = 0; idx < TS_State.touchDetected; idx++) {
                 x = TS_State.touchX[idx];
                 y = TS_State.touchY[idx];
-                if(x >= setX && x <= setX + setWidth && y >= setY && y <= setY + setHeight) {
-                    BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"Bylo zmacknut set", CENTER_MODE);
-                } else if (x >= incDecX && x <= incDecX + incDecSize && y >= incY && y <= incY + incDecSize){
-                    BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"Byl zmacknut plus!", CENTER_MODE);
-                } else if (x >=incDecX && x <= incDecX + incDecSize && y >= decY && y <= decY + incDecSize) {
-                    BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"Byl zmacknut minus", CENTER_MODE);
+                if(x >= incDecX && x <= incDecX + incDecSize && y >= incY && y <= incY + incDecSize && isTouch == 0) {
+                    timeToAdd += 10;
+                } else if (x >= incDecX && x <= incDecX + incDecSize && y >= decY && y <= decY + incDecSize && isTouch == 0) {
+                    timeToAdd -= 10;
                 }
-            }         
+                isTouch = 1;
+            }
+        } else {
+            isTouch = 0;    
         }
     }
 }
