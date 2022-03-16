3c94f9d843aa1d9cb2f8fee314134910eb4ae1da
kareldrnec <70096530+kareldrnec@users.noreply.github.com>
2022-03-16 13:11:00 +0100

    Update main.cpp
    update

--- 02 bomba/main.cpp
+++ 02 bomba/main.cpp
@@ -74,3 +74,93 @@ void display()
 }
 
 
+void set()
+{
+    uint16_t x, y;
+    uint8_t idx;
+    BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"Tlacitko", CENTER_MODE);
+    BSP_TS_GetState(&TS_State);
+    if (TS_State.touchDetected) {
+        for(idx = 0; idx < TS_State.touchDetected; idx++) {
+            x = TS_State.touchX[idx];
+            y = TS_State.touchY[idx];
+            if (x >= incDecX && x <= incDecX + incDecSize && y >= incY && y <= incY + incDecSize) {
+                BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"Byl zmacknut plus!", CENTER_MODE);
+            } else if (x >=incDecX && x <= incDecX + incDecSize && y >= decY && y <= decY + incDecSize) {
+                BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"Byl zmacknut minus", CENTER_MODE);
+            }
+        }
+    }
+}
+
+
+int main()
+{
+    int timeValue;
+    uint16_t x, y;
+    uint8_t text[30];
+    uint8_t status;
+    uint8_t idx;
+    uint8_t cleared = 0;
+    uint8_t prev_nb_touches = 0;
+
+    BSP_LCD_Init();
+    BSP_LCD_LayerDefaultInit(LTDC_ACTIVE_LAYER, LCD_FB_START_ADDRESS);
+    BSP_LCD_SelectLayer(LTDC_ACTIVE_LAYER);
+
+    BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"BOMBA DEMO", CENTER_MODE);
+    HAL_Delay(1000);
+
+    status = BSP_TS_Init(BSP_LCD_GetXSize(), BSP_LCD_GetYSize());
+    if (status != TS_OK) {
+        BSP_LCD_Clear(LCD_COLOR_RED);
+        BSP_LCD_SetBackColor(LCD_COLOR_RED);
+        BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+        BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"TOUCHSCREEN INIT FAIL", CENTER_MODE);
+    } else {
+        BSP_LCD_Clear(LCD_COLOR_GREEN);
+        BSP_LCD_SetBackColor(LCD_COLOR_BLUE);
+        BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+        BSP_LCD_DisplayStringAt(0, LINE(5), (uint8_t *)"TOUCHSCREEN INIT OK", CENTER_MODE);
+    }
+    HAL_Delay(1000);
+    BSP_LCD_Clear(LCD_COLOR_WHITE);
+    BSP_LCD_SetFont(&Font24);
+    BSP_LCD_SetBackColor(LCD_COLOR_WHITE);
+    BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+
+    // draw buttons
+    drawButtons();
+
+    // draw bomba
+    drawBomb();
+
+    flipper.attach(&display, 100s);
+
+    button.rise(&set);
+
+    while(1) {
+        // print zbyvajiciho casu
+        timeValue = duration_cast<seconds>(flipper.remaining_time()).count() + 1;
+        BSP_LCD_DisplayStringAt(0, LINE(1), (uint8_t *)"Remaining Time", CENTER_MODE);
+
+        sprintf((char*)text, "  %llu  ", duration_cast<seconds>(flipper.remaining_time()).count() + 1);
+
+        BSP_LCD_DisplayStringAt(0, LINE(2), (uint8_t *)&text, CENTER_MODE);
+
+        // touch detect
+        BSP_TS_GetState(&TS_State);
+        if (TS_State.touchDetected) {
+            for(idx = 0; idx < TS_State.touchDetected; idx++) {
+                x = TS_State.touchX[idx];
+                y = TS_State.touchY[idx];
+                if(x >= setX && x <= setX + setWidth && y >= setY && y <= setY + setHeight) {
+                    BSP_LCD_DisplayStringAt(0, LINE(4), (uint8_t *)"Bylo zmacknut set", CENTER_MODE);
+                }
+            }
+        }
+    }
+}
+
+
+
